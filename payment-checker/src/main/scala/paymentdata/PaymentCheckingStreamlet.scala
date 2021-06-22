b90bc06dc8f98de0201815839185afcac4886262
GeorgeII <egorzorikov@gmail.com>
2021-06-22 17:55:38 +0400

    Inefficient double filtering replaced with Flink's native sideOutput technique

--- payment-checker/src/main/scala/paymentdata/PaymentCheckingStreamlet.scala
+++ payment-checker/src/main/scala/paymentdata/PaymentCheckingStreamlet.scala
@@ -3,7 +3,9 @@ package paymentdata
 import cloudflow.flink.{ FlinkStreamlet, FlinkStreamletLogic }
 import cloudflow.streamlets.StreamletShape
 import cloudflow.streamlets.avro.{ AvroInlet, AvroOutlet }
-import org.apache.flink.streaming.api.scala.{ createTypeInformation, DataStream }
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.flink.streaming.api.scala.{ createTypeInformation, DataStream, OutputTag }
+import org.apache.flink.util.Collector
 import paymentdata.utils.PaymentUtils.{ extractParticipantsAndValue, isValid }
 
 class PaymentCheckingStreamlet extends FlinkStreamlet {
@@ -21,19 +23,27 @@ class PaymentCheckingStreamlet extends FlinkStreamlet {
     override def buildExecutionGraph(): Unit = {
       val transfers: DataStream[Transfer] = readStream(in)
 
+      val invalidTransfersOutputTag = OutputTag[InvalidTransfer]("invalid")
+
       val validPayments =
         transfers
-          .filter(transfer => isValid(transfer.data))
-          .map(transfer =>
-            extractParticipantsAndValue(transfer.data) match {
-              case (p1, p2, value) => ParsedPayment(p1, p2, value)
-            }
-          )
+          .process(new ProcessFunction[Transfer, ParsedPayment] {
+            override def processElement(transfer: Transfer,
+                                        ctx: ProcessFunction[Transfer, ParsedPayment]#Context,
+                                        out: Collector[ParsedPayment]): Unit = {
+              if (isValid(transfer.data)) {
+                val parsedPayment = extractParticipantsAndValue(transfer.data) match {
+                  case (p1, p2, value) => ParsedPayment(p1, p2, value)
+                }
 
-      val invalidTransfers =
-        transfers
-          .filter(transfer => !isValid(transfer.data))
-          .map(transfer => InvalidTransfer(transfer.data, "IncorrectAttributes"))
+                out.collect(parsedPayment)
+              } else {
+                ctx.output(invalidTransfersOutputTag, InvalidTransfer(transfer.data, "IncorrectAttributes"))
+              }
+            }
+          })
+
+      val invalidTransfers = validPayments.getSideOutput(invalidTransfersOutputTag)
 
       writeStream(outPayment, validPayments)
       writeStream(outInvalid, invalidTransfers)
